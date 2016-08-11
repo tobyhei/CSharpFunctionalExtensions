e015e605b6bcfc8e347395626e577faafc1baf70
Toby Heighway <toby.heighway@domain.com.au>
2016-08-11 17:18:11 +1000

    Added ValueObjectList with tests

---
+++ CSharpFunctionalExtensions.Tests/ValueObjectListTests/ValueObjectListTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpFunctionalExtensions.Tests.ValueObjectListTests
+{
+    public class ValueObjectListTests
+    {
+        [Fact]
+        public void Can_create_an_empty_value_object_list()
+        {
+            var valueObjectList = new ValueObjectList<MyValueObject>(new List<MyValueObject>());
+
+            valueObjectList.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Can_create_a_multiple_element_value_object_list()
+        {
+            var valueObjectList = ListFromTestValues(new[] { 5, 7, 6 });
+
+            valueObjectList.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public void Two_sorted_equal_value_object_lists_are_equal()
+        {
+            var valueObjectList = ListFromTestValues(new[] { 5, 6, 7 });
+
+            var valueObjectList2 = ListFromTestValues(new[] { 5, 6, 7 });
+
+            valueObjectList.Should().BeEquivalentTo(valueObjectList2);
+        }
+
+        [Fact]
+        public void Two_unsorted_equal_value_object_lists_are_equal()
+        {
+            var valueObjectList = ListFromTestValues(new[] { 5, 7, 6 });
+
+            var valueObjectList2 = ListFromTestValues(new[] { 6, 5, 7 });
+
+            valueObjectList.Should().BeEquivalentTo(valueObjectList2);
+        }
+
+        [Fact]
+        public void Two_unequal_value_object_lists_are_not_equal()
+        {
+            var valueObjectList = ListFromTestValues(new[] { 5, 7, 6 });
+
+            var valueObjectList2 = ListFromTestValues(new[] { 5, 6, 4 });
+
+            valueObjectList.Should().NotBeEquivalentTo(valueObjectList2);
+        }
+
+        [Fact]
+        public void Two_unequal_length_value_object_lists_are_not_equal()
+        {
+            var valueObjectList = ListFromTestValues(new[] {5, 7});
+
+            var valueObjectList2 = ListFromTestValues(new[] { 5, 7, 4});
+
+            valueObjectList.Should().NotBeEquivalentTo(valueObjectList2);
+        }
+
+        private ValueObjectList<MyValueObject> ListFromTestValues(IEnumerable<int> testValues)
+        {
+            return new ValueObjectList<MyValueObject>(testValues.Select(x => new MyValueObject(x)));
+        }
+
+        public class MyValueObject : ValueObject<MyValueObject>, IComparable<MyValueObject>
+        {
+            public int Value { get; }
+
+            public MyValueObject(int value)
+            {
+                Value = value;
+            }
+
+            protected override bool EqualsCore(MyValueObject other)
+            {
+                return other.Value == Value;
+            }
+
+            protected override int GetHashCodeCore()
+            {
+                return Value.GetHashCode();
+            }
+
+            public int CompareTo(MyValueObject other)
+            {
+                return Value.CompareTo(other.Value);
+            }
+        }
+    }
+}
---
+++ CSharpFunctionalExtensions/ValueObjectList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpFunctionalExtensions
+{
+    public class ValueObjectList<T> : ValueObject<ValueObjectList<T>>, IEnumerable<T>
+        where T : ValueObject<T>, IComparable<T>
+    {
+        public List<T> Items { get; }
+
+        public ValueObjectList(IEnumerable<T> items)
+        {
+            Items = items.ToList();
+        }
+
+        protected override bool EqualsCore(ValueObjectList<T> other)
+        {
+            return Items.OrderBy(x => x).SequenceEqual(other.Items.OrderBy(x => x));
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Items.Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
